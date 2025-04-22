6d608a941bf14f80f18d6812f438276fabca2972
Elizaveta Strelnikova <elizaveta.strelnikova357@gmail.com>
2025-04-22 22:46:17 +0300

    add abstract model for log file

---
+++ Models/LogFile.cs
@@ -0,0 +1,27 @@
+﻿using LogStandartization.Enums;
+
+namespace LogStandartization.Models
+{
+    public abstract class LogFile
+    {
+        DateTime Date { get; set; }
+        LoggingLevel LoggingLevel { get; set; }
+
+        string CallingMethod { get; set; } = "DEFAULT";
+        string Message { get; set; }
+
+        public void Standartize(string path)
+        {
+            Get(path);
+            Validate();
+            Save();
+        }
+
+        public abstract void Get(string path);
+        public abstract void Validate();
+        public virtual void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
